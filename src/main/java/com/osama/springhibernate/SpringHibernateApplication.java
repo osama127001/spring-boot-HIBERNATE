0768f59ea00274abd4d5ef97a660a996467e23cb
Osama Khan <52834187+gitosamakhan@users.noreply.github.com>
2021-01-27 12:01:41 +0500

    initial commit

---
+++ src/main/java/com/osama/springhibernate/SpringHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.osama.springhibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHibernateApplication.class, args);
+	}
+
+}
